9a97c13fc92e99fe6be4d3c29d4c7e4c03dfa96d
chacha <867571123@qq.com>
2020-11-15 18:01:05 +0800

    feat: add grid view adapter

---
+++ app/src/main/java/com/example/awesomeandroidapp/adapter/GridViewAdapter.java
@@ -0,0 +1,41 @@
+package com.example.awesomeandroidapp.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class GridViewAdapter extends BaseAdapter {
+    private int[] list;
+    private Context context;
+
+    public GridViewAdapter(Context context, int[] gridList) {
+        this.context = context;
+        this.list = gridList;
+    }
+
+    @Override
+    public int getCount() {
+        return list.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ImageView imageView = new ImageView(context);
+        imageView.setImageResource(list[i]);
+        imageView.setAdjustViewBounds(true);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        return imageView;
+    }
+}
