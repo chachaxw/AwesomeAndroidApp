1b42ee688c02b5fcb611572cc48ad0374930a2f5
chacha <867571123@qq.com>
2020-11-25 14:47:54 +0800

    feat: add a bitmap transform handler thread in HomeFragment

--- app/src/main/java/com/example/awesomeandroidapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/awesomeandroidapp/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.awesomeandroidapp.ui.home;
 
+import android.Manifest;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -14,21 +15,21 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.Observer;
 
 import com.example.awesomeandroidapp.R;
 import com.sch.share.WXShareMultiImageHelper;
 import com.example.awesomeandroidapp.adapter.GridViewAdapter;
+import com.tbruyelle.rxpermissions3.RxPermissions;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Objects;
-import java.util.logging.Logger;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RxPermissions rxPermissions = new RxPermissions(this);
     private static final String TAG = HomeFragment.class.getSimpleName();
     private int[] imgList = {
         R.mipmap.img_1,
@@ -44,8 +45,7 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         Button shareButton = root.findViewById(R.id.shareButton);
         shareButton.setOnClickListener(new ButtonOnClickListener());
@@ -60,14 +60,24 @@ public class HomeFragment extends Fragment {
             Toast toast = Toast.makeText(getContext(), "这是匿名内部类作为事件监听器类", Toast.LENGTH_LONG);
             toast.setGravity(Gravity.CENTER, 0, 0);
             toast.show();
-            shareToFriend();
+            rxPermissions
+                    .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    .subscribe(granted -> {
+                        if (granted) {
+                            shareToFriend((List<Bitmap> bitmapList) -> {
+                                Log.d(TAG, "分享数据" + bitmapList);
+                            });
+                        } else {
+                            Log.d(TAG, "未开启文件写入权限！");
+                        }
+                    });
         }
     }
 
     @Override
     public void onDestroy() {
+        WXShareMultiImageHelper.clearTmpFile(requireContext());
         super.onDestroy();
-        WXShareMultiImageHelper.clearTmpFile(Objects.requireNonNull(getContext()));
     }
 
     private void initGridView(View root) {
@@ -78,39 +88,41 @@ public class HomeFragment extends Fragment {
     }
 
     /** Called when the user taps the button */
-    private void shareToFriend() {
-//        WXShareMultiImageHelper.shareToSession((getActivity(), );
+    private void shareToFriend(final OnLoadImageEndCallback callback) {
         Log.d(TAG, "分享给好友！");
-    }
-
-    private void share() {
-        new Thread(new Task("Thread-1")).start();
+        new Thread(new Task("Thread-1", callback)).start();
     }
 
     private class Task implements Runnable {
         private String threadName;
+        private OnLoadImageEndCallback onLoadImageEnd;
 
-        Task(String name) {
+        Task(String name, OnLoadImageEndCallback callback) {
             threadName = name;
+            onLoadImageEnd = callback;
             Log.d(TAG, "线程名称" + threadName);
         }
 
         @Override
         public void run() {
             try {
-                Log.d(TAG, "多线程处理运行开始");
-                ArrayList<Bitmap> bitmapList = new ArrayList<Bitmap>();
+                Log.d(TAG, "多线程处理运行开始" + threadName);
+                List<Bitmap> bitmapList = new ArrayList<>();
 
                 for (int item : imgList) {
                     Bitmap bitmap = BitmapFactory.decodeResource(getResources(), item);
                     bitmapList.add(bitmap);
                 }
 
-//                WXShareMultiImageHelper.shareToSession(getActivity(), bitmapList);
-                Log.d(TAG, "多线程处理运行结束");
+                onLoadImageEnd.onEnd(bitmapList);
+                Log.d(TAG, "多线程处理运行结束" + threadName);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private interface OnLoadImageEndCallback {
+        void onEnd(List<Bitmap> bitmapList);
     }
 }
--- app/src/main/java/com/example/awesomeandroidapp/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/awesomeandroidapp/ui/notifications/NotificationsFragment.java
@@ -10,9 +10,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.awesomeandroidapp.R;
+import com.example.awesomeandroidapp.ui.dashboard.DashboardViewModel;
 
 public class NotificationsFragment extends Fragment {
 
@@ -20,8 +22,7 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
