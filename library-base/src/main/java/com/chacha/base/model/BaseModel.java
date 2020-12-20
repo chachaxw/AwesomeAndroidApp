05bd81a3354fcb3da5f52423c948a3109d06ce4e
chacha <867571123@qq.com>
2020-12-20 11:18:45 +0800

    feat: add BaseModel abstract class

---
+++ library-base/src/main/java/com/chacha/base/model/BaseModel.java
@@ -0,0 +1,28 @@
+package com.chacha.base.model;
+
+/**
+ * @author chacha
+ * @project AwesomeAndroidApp
+ * @package_name com.chacha.base.model
+ * @date 12/17/20
+ * @time 4:35 PM
+ */
+public abstract class BaseModel<T> extends SuperBaseModel<T> {
+    /**
+     * 网络数据加载成功，通知所有注册者加载数据
+     *
+     * @param data 数据bean
+     */
+    public void loadSuccess(T data) {
+
+        /**
+         * synchronized（用来修饰方法或者代码块）
+         * 1. synchronized 是同步锁，用来实现互斥同步。
+         * 2. 关键字synchronized 可以保证在同一个时刻，只有一个线程可以执行某个方法或者某个代码块（主要是对方法或者代码块中存在共享数据的操作）
+         * 3. synchronized 还可以保证一个线程的变化（主要是共享数据的变化）被其他线程所看到
+         */
+        synchronized (this) {
+
+        }
+    }
+}
