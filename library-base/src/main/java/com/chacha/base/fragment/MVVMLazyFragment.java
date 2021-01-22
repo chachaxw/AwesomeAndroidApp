eb0bc2db9ff496fedb8e9f1de535e8ebf903b216
chacha <867571123@qq.com>
2021-01-23 00:11:47 +0800

    feat: add MVVMLazyFragment base class

--- library-base/src/main/java/com/chacha/base/fragment/MVVMLazyFragment.java
+++ library-base/src/main/java/com/chacha/base/fragment/MVVMLazyFragment.java
@@ -8,8 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.chacha.base.activity.IBaseView;
+import com.chacha.base.loadsir.EmptyCallback;
+import com.chacha.base.loadsir.ErrorCallback;
+import com.chacha.base.loadsir.LoadingCallback;
+import com.chacha.base.utils.ToastUtils;
 import com.chacha.base.viewmodel.IMVVMBaseViewModel;
+import com.kingja.loadsir.callback.Callback;
 import com.kingja.loadsir.core.LoadService;
+import com.kingja.loadsir.core.LoadSir;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
@@ -24,6 +30,11 @@ import androidx.fragment.app.Fragment;
  * @package_name com.chacha.base.fragment
  * @date 1/17/21
  * @time 10:54 PM
+ *
+ * <a href="https://developer.android.com/guide/components/fragments?hl=zh-cn>
+ *     Fragment片段官方文档
+ * </a>
+ *
  */
 public abstract class MVVMLazyFragment<V extends ViewDataBinding, VM extends IMVVMBaseViewModel> extends Fragment implements IBaseView {
 
@@ -45,6 +56,8 @@ public abstract class MVVMLazyFragment<V extends ViewDataBinding, VM extends IMV
 
     // 布局是否完成
     protected boolean isViewCreated = false;
+
+    private boolean isShowedContent = false;
 
     // 当前可见状态
     protected boolean currentVisibleState = false;
@@ -108,9 +121,28 @@ public abstract class MVVMLazyFragment<V extends ViewDataBinding, VM extends IMV
         }
     }
 
+    /**
+     * 修改Fragment的可见性，setUserVisibleHint被调用有两种情况：
+     * 1) 在切换tab的时候，会先于所有Fragment的其他生命周期，先调用这个函数，可以看log
+     * 2) 对于之前已经调用过setUserVisibleHint方法的fragment后，让fragment从可见到不可见之间状态的变化
+     *
+     * @param isVisibleToUser 是否对用户可见
+     */
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
+        Log.d(fragmentTag,"setUserVisibleHint: " + isVisibleToUser);
+
+        if (isViewCreated) {
+            // 必须是可见的（isVisibleToUser为true）而且只有当可见状态进行改变的时候才需要切换，
+            // 否则会出现反复调用的情况，从而导致事件分发带来的多次更新
+            if (isVisibleToUser && !currentVisibleState) {
+                // 从不可见 -> 可见
+                dispatchUserVisibleHint(true);
+            } else if (!isVisibleToUser && currentVisibleState) {
+                dispatchUserVisibleHint(false);
+            }
+        }
     }
 
     /**
@@ -120,10 +152,179 @@ public abstract class MVVMLazyFragment<V extends ViewDataBinding, VM extends IMV
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
+        Log.d(fragmentTag,"onHiddenChanged: " + hidden);
+
+        if (hidden) {
+            dispatchUserVisibleHint(false);
+        } else {
+            dispatchUserVisibleHint(true);
+        }
     }
 
+    /**
+     * 统一处理用户可见事件分发
+     *
+     * @param isVisible 是否可见
+     */
     private void dispatchUserVisibleHint(boolean isVisible) {
+        Log.d(fragmentTag,"dispatchUserVisibleHint: " + isVisible);
 
+        // 首先考虑一下fragment嵌套fragment的情况(只考虑2层嵌套)
+        if (isVisible && isParentInvisible()) {
+            // 父Fragment此时不可见，直接return不做处理
+            return;
+        }
+
+        // 如果当前状态与需要设置的状态本来就一致了，就不处理了
+        if (isVisible == currentVisibleState) {
+            return;
+        }
+
+        currentVisibleState = isVisible;
+
+        if (isVisible) {
+            if (isFirstVisible) {
+                isFirstVisible = false;
+                // 第一次可见,进行全局初始化
+                onFragmentFirstVisible();
+            }
+
+            onFragmentResume();
+
+            // 分发事件给内嵌的Fragment
+            dispatchUserVisibleHint(true);
+        } else {
+            onFragmentPause();
+            dispatchUserVisibleHint(false);
+        }
+    }
+
+    // Fragment真正的Pause，暂停一切网络耗时操作
+    private void onFragmentPause() {
+        Log.d(fragmentTag,"onFragmentPause: " + "暂停并结束相关操作耗时");
+    }
+
+    // Fragment真正的Resume，开始处理网络加载等耗时操作
+    private void onFragmentResume() {
+        Log.d(fragmentTag,"onFragmentPause: " + "恢复并开始相关操作耗时");
+    }
+
+    // Fragment第一次可见，根据业务进行初始化操作
+    private void onFragmentFirstVisible() {
+        Log.d(fragmentTag,"onFragmentFirstVisible  第一次可见");
+    }
+
+    // 父Fragment是否不可见
+    private boolean isParentInvisible() {
+        Fragment parentFragment = getParentFragment();
+
+        if (parentFragment instanceof MVVMLazyFragment) {
+            MVVMLazyFragment fragment = (MVVMLazyFragment) parentFragment;
+            return !fragment.isSupportVisible();
+        }
+
+        return false;
+    }
+
+    private boolean isSupportVisible() {
+        return currentVisibleState;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // 如果不是第一次可见
+        if (!isFirstVisible) {
+            // 如果此时进行Activity跳转，会将所有缓存的Fragment进行onResume
+            // 只需要对可见的Fragment进行加载
+            if (!isHidden() && !currentVisibleState && getUserVisibleHint()) {
+                dispatchUserVisibleHint(true);
+            }
+        }
+    }
+
+    /**
+     * 只有当当前状态由可见状态转变到不可见状态时才需要调用 dispatchUserVisibleHint，
+     * currentVisibleState && getUserVisibleHint() 能够限定时当前可见的Fragment，
+     * 当前Fragment包含子Fragment的时候，dispatchUserVisibleHint内部本身就会
+     * 通知子Fragment走到这里的时候自身又会调用一遍。
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (currentVisibleState && getUserVisibleHint()) {
+            dispatchUserVisibleHint(false);
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        isViewCreated = false;
+        Log.d(fragmentTag,"onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (viewModel != null && viewModel.isUIAttached()) {
+            viewModel.detachUI();
+        }
+
+        Log.d(fragmentTag,"onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(fragmentTag,"onDetach");
+    }
+
+    /**
+     * 注册LoadSir, loadsir使用说明(https://github.com/KingJA/LoadSir)
+     *
+     * @param view 界面View
+     */
+    public void setLoadSir(View view) {
+        if (loadService == null) {
+            loadService = LoadSir.getDefault().register(view, (Callback.OnReloadListener) v -> onRetryButtonClick());
+        }
+    }
+
+    @Override
+    public void showContent() {
+        if (loadService != null) {
+            isShowedContent = true;
+            loadService.showSuccess();
+        }
+    }
+
+    @Override
+    public void showLoading() {
+        if (loadService != null) {
+            loadService.showCallback(LoadingCallback.class);
+        }
+    }
+
+    @Override
+    public void showEmpty() {
+        if (loadService != null) {
+            loadService.showCallback(EmptyCallback.class);
+        }
+    }
+
+    @Override
+    public void showError(String message) {
+        if (loadService != null) {
+            if (!isShowedContent) {
+                loadService.showCallback(ErrorCallback.class);
+            } else {
+                ToastUtils.show(getContext(), message);
+            }
+        }
     }
 
     // 获取参数Variable
