91484e9e49776753e6acd00d7d58bbc5edaa0f31
chacha <867571123@qq.com>
2021-02-09 17:44:16 +0800

    feat: add GlobalKey class constant

---
+++ library-common/src/main/java/com/chacha/common/global/GlobalKey.java
@@ -0,0 +1,21 @@
+package com.chacha.common.global;
+
+/**
+ * @author chacha
+ * @project AwesomeAndroidApp
+ * @package_name com.chacha.common.global
+ * @date 2/9/21
+ * @time 5:32 PM
+ *
+ * 应用模块: 全局配置
+ * <p>
+ *     全局存储的key，单个组件的可以单个组件自行在各自的组件中定义
+ * </p>
+ */
+public class GlobalKey {
+    // 用户信息
+    public static final String USER_INFO = "user_info";
+
+    // 令牌信息
+    public static final String APP_TOKEN = "app_token";
+}
