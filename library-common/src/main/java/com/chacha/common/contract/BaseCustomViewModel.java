f4bdcdf446483ea8b70efa091800362d17485bde
chacha <867571123@qq.com>
2021-02-10 15:49:45 +0800

    feat: add VideoHeaderBean class and BaseCustomViewModel

---
+++ library-common/src/main/java/com/chacha/common/contract/BaseCustomViewModel.java
@@ -0,0 +1,17 @@
+package com.chacha.common.contract;
+
+import java.io.Serializable;
+
+/**
+ * @author chacha
+ * @project AwesomeAndroidApp
+ * @package_name com.chacha.common.contract
+ * @date 2/10/21
+ * @time 3:33 PM
+ *
+ * <p>
+ *      用于各个组件之间公用的契约类，需要共同遵守相关规定
+ * <p>
+ */
+public class BaseCustomViewModel implements Serializable {
+}
